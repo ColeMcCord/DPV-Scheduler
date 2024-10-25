65266aba8551f4fb916b0cf30d57d649fe0e211f
ColeMcCord <mccord.cole@gmail.com>
2024-10-25 18:29:34 -0400

    Added Initial Files

---
+++ Project.py
@@ -0,0 +1,28 @@
+class Project:
+    def __init__(self, name, team):
+        self.name = name
+        self.team = team
+        self.task_list = []
+
+    def add_task(self, task):
+        self.task_list.append(task)
+        self.sort_tasks()
+
+    def sort_tasks(self):
+        raise NotImplementedError
+
+    def remove_task(self, task):
+        raise NotImplementedError
+
+    def finish_task(self, task):
+        raise NotImplementedError
+
+    def pop_task(self):
+        raise NotImplementedError
+
+    def edit_task(self):
+        raise NotImplementedError
+
+    def update(self):
+        raise NotImplementedError
+
---
+++ User.py
@@ -0,0 +1,50 @@
+
+class InvalidAccessError(Exception):
+    def __init__(self, type):
+        self.type = type
+
+    def __str__(self):
+        return f"You do not have access to do {self.type}"
+
+
+class User:
+    def __init__(self, name, salary=0):
+        self.salary = salary
+        self.name = name
+
+    def change_salary(self, new_salary):
+        self.salary = new_salary
+
+    def get_salary(self):
+        return self.salary
+
+    def get_name(self):
+        return self.name
+
+
+
+class Admin(User):
+    def __init__(self, name, salary=0):
+        super.__init__(name, salary)
+
+    def change_required_return(self, required_return):
+        raise NotImplementedError
+
+class Manager(User):
+
+
+    def __init__(self, name, can_see_salaries = True, salary=0):
+        self.can_see_salaries = can_see_salaries
+        super.__init__(name, salary)
+
+
+    def get_salary(self, employee):
+        if self.can_see_salaries = False:
+            raise
+
+
+    raise NotImplementedError
+
+
+class Employee(User):
+    raise NotImplementedError
